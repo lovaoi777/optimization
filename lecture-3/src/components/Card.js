b12c686868594462cd2f87bcdb6d20deabc2747f
lovaoi777 <lovaoi777@naver.com>
2023-04-07 21:01:29 +0900

    최적화

--- lecture-3/src/components/Card.js
+++ lecture-3/src/components/Card.js
@@ -1,14 +1,29 @@
-import React from 'react'
-
+import React, { useRef, useEffect } from "react";
 function Card(props) {
-	return (
-		<div className="Card text-center">
-			<img src={props.image}/>
-			<div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">
-				{props.children}
-			</div>
-		</div>
-	)
+  const ImgRef = useRef(null);
+  const callback = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        console.log("checking img");
+        entry.target.src = entry.target.dataset.src;
+      }
+    });
+  };
+  const options = () => {};
+
+  const observer = new IntersectionObserver(callback, options);
+  useEffect(() => {
+    observer.observe(ImgRef.current);
+  }, []);
+
+  return (
+    <div className="Card text-center">
+      <img data-src={props.image} ref={ImgRef} />
+      <div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">
+        {props.children}
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
--- lecture-3/src/components/Header.js
+++ lecture-3/src/components/Header.js
@@ -29,7 +29,7 @@ function Header(props) {
 								<svg className="fill-current w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
 									<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
 								</svg>
-							</button>
+							</button> 
 							<div className="hidden lg:flex lg:items-center lg:justify-between px-6">
 								<div className="flex justify-start items-center text-gray-500">
 									<Link className="block flex items-center hover:text-gray-700 mr-5" to={'/'} >
